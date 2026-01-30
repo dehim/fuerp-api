ea37047f41c549b1ab6ba0df9dff6da604ab923a
dehim <dehim@qq.com>
2026-01-30 22:46:15 +0800

    main-local.php 配置 redis

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -9,6 +9,16 @@ return [
             'password' => '',
             'charset' => 'utf8',
         ],
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => '127.0.0.1',
+            'port' => 6379,
+            'database' => 0,
+
+            // 可选但推荐
+            'retries' => 1,
+            'retryInterval' => 100,
+        ],
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
             'viewPath' => '@common/mail',
