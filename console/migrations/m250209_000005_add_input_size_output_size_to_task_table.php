fe8e5922a871e769b8c90c3830bfdc540704296b
dehim <dehim@qq.com>
2026-02-09 21:40:20 +0800

    修正bigInteger的正确表达

--- console/migrations/m250209_000005_add_input_size_output_size_to_task_table.php
+++ console/migrations/m250209_000005_add_input_size_output_size_to_task_table.php
@@ -7,10 +7,9 @@ class m250209_000005_add_input_size_output_size_to_task_table extends Migration
     public function up()
     {
         // 原始大小，bytes
-        $this->addColumn('{{%task}}', 'input_size', $this->bigInteger->defaultValue(null));
+        $this->addColumn('{{%task}}', 'input_size', $this->bigInteger()->defaultValue(null)->comment('原始文件大小（bytes）'));
         // 处理后大小，bytes
-        $this->addColumn('{{%task}}', 'output_size', $this->bigInteger->defaultValue(null));
-
+        $this->addColumn('{{%task}}', 'output_size', $this->bigInteger()->defaultValue(null)->comment('处理后文件大小（bytes）'));
     }
 
     public function down()
