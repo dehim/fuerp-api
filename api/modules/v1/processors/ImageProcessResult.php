a5a50e83ab9bab57b49056bc550f498505ed4f77
dehim <dehim@qq.com>
2026-02-10 13:39:15 +0800

    修正PHP parser 级别的报错，兼容 PHP 7.4 / 8.0 / 8.1 / 8.2 / 8.4

--- api/modules/v1/processors/ImageProcessResult.php
+++ api/modules/v1/processors/ImageProcessResult.php
@@ -4,9 +4,12 @@ namespace api\modules\v1\processors;
 
 class ImageProcessResult
 {
-    public function __construct(
-        public string $outputPath,
-        public int    $outputSize,
-    ) {
+    public string $outputPath;
+    public int $outputSize;
+
+    public function __construct(string $outputPath, int $outputSize)
+    {
+        $this->outputPath = $outputPath;
+        $this->outputSize = $outputSize;
     }
 }
